866fe4b29c348fce0e0f71fa576f08a89174ed2c
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-01-22 21:23:26 -0300

    Removed console.log()

--- assets/scripts/hover.js
+++ assets/scripts/hover.js
@@ -3,12 +3,10 @@ export function hover() {
     card.forEach((element) => {
         element.addEventListener('mouseenter', () => {
             element.querySelector('.project__footer').classList.add('is-active')
-            console.log('1')
         })
 
         element.addEventListener('mouseleave', () => {
             element.querySelector('.project__footer').classList.remove('is-active')
-            console.log('2')
         })
         
     })
